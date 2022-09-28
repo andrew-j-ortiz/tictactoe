638f95be975d6ba88af14d092dc3f5733abc5889
kodeb8 <andrewjavierortiz@gmail.com>
2022-09-28 17:18:31 -0400

    Bug fixes, UI fixes, add game restart

--- src/App.jsx
+++ src/App.jsx
@@ -13,31 +13,56 @@ function App() {
     gridNine: ""
   })
   const [currentPlayer, setCurrentPlayer] = useState("X")
+  const [gameWon, setGameWon] = useState(false)
   const [winner, setWinner] = useState("")
+  const [cpuEnabled, setCpuEnabled] = useState(false)
 
   useEffect(()=>{
-    const rowOne = [board.gridOne, board.gridTwo, board.gridThree].every( e => checkForWinner(e))
-    const rowTwo = [board.gridFour, board.gridFive, board.gridSix].every( e => checkForWinner(e))
-    const rowThree = [board.gridSeven, board.gridEight, board.gridNine].every( e => checkForWinner(e))
-
-    const columnOne = [board.gridOne, board.gridFour, board.gridSeven].every( e => checkForWinner(e))
-    const columnTwo = [board.gridTwo, board.gridFive, board.gridEight].every( e => checkForWinner(e))
-    const columnThree = [board.gridThree, board.gridSix, board.gridNine].every( e => checkForWinner(e))
-
-    const diagonalOne = [board.gridOne, board.gridFive, board.gridNine].every( e => checkForWinner(e))
-    const diagonalTwo = [board.gridThree, board.gridFive, board.gridSeven].every( e => checkForWinner(e))
-
-    function checkForWinner(e) {
-      if (e === "X") {
+    const rowOne = [board.gridOne, board.gridTwo, board.gridThree].every(e => {
+      if (e !== "" && e === board.gridOne) {
         return true
-      } else if (e === "O") {
-        return true
-      } else {
-        return false
       }
-    }
+    })
+    const rowTwo = [board.gridFour, board.gridFive, board.gridSix].every(e => {
+      if (e !== "" && e === board.gridFour) {
+        return true
+      }
+    })
+    const rowThree = [board.gridSeven, board.gridEight, board.gridNine].every(e => {
+      if (e !== "" && e === board.gridSeven) {
+        return true
+      }
+    })
+
+    const columnOne = [board.gridOne, board.gridFour, board.gridSeven].every(e => {
+      if (e !== "" && e === board.gridOne) {
+        return true
+      }
+    })
+    const columnTwo = [board.gridTwo, board.gridFive, board.gridEight].every(e => {
+      if (e !== "" && e === board.gridTwo) {
+        return true
+      }
+    })
+    const columnThree = [board.gridThree, board.gridSix, board.gridNine].every(e => {
+      if (e !== "" && e === board.gridThree) {
+        return true
+      }
+    })
+
+    const diagonalOne = [board.gridOne, board.gridFive, board.gridNine].every(e => {
+      if (e !== "" && e === board.gridOne) {
+        return true
+      }
+    })
+    const diagonalTwo = [board.gridThree, board.gridFive, board.gridSeven].every(e => {
+      if (e !== "" && e === board.gridThree) {
+        return true
+      }
+    })
 
     if (rowOne || rowTwo || rowThree || columnOne || columnTwo || columnThree || diagonalOne || diagonalTwo) {
+      setGameWon(true)
       if (currentPlayer === "X") {
         setWinner("O")
       } else if (currentPlayer === "O") {
@@ -46,9 +71,13 @@ function App() {
     }
   }, [board])
 
-  useEffect(()=>{
-    console.log("Game over! " + winner + " wins!")
-  }, [winner])
+  useEffect(()=> {
+    if (gameWon) {
+      setTimeout(()=>{
+        resetGame()
+      }, 3000)
+    }
+  }, [gameWon])
 
   const getGrid = (rowsIndex, index) => {
     let grid = ""
@@ -88,20 +117,22 @@ function App() {
   const handleClick = (gridArray) => {
     const gridSpace = gridArray[1]
 
-    setBoard(prevBoard => {
-      return {
-        ...prevBoard,
-        [gridSpace]: currentPlayer
-      }
-    })
+    if (gridArray[0] === "") {
+      setBoard(prevBoard => {
+        return {
+          ...prevBoard,
+          [gridSpace]: currentPlayer
+        }
+      })
 
-    setCurrentPlayer(prevPlayer => {
-      if (prevPlayer === "X") {
-        return "O"
-      } else if (prevPlayer === "O") {
-        return "X"
-      }
-    })
+      setCurrentPlayer(prevPlayer => {
+        if (prevPlayer === "X") {
+          return "O"
+        } else if (prevPlayer === "O") {
+          return "X"
+        }
+      })
+    }
 
   }
 
@@ -114,9 +145,7 @@ function App() {
       gridsArray.push(
         <div 
           className="board--grid"
-          onClick={
-            ()=>handleClick(gridArray)
-          }
+          onClick={()=>{ if (!gameWon) {handleClick(gridArray)} }}
         >
           <p className="letter">
             {grid}
@@ -139,19 +168,51 @@ function App() {
     return rowsArray
   }
 
+  const resetGame = () => {
+    setBoard({
+      gridOne: "",
+      gridTwo: "",
+      gridThree: "",
+      gridFour: "",
+      gridFive: "",
+      gridSix: "",
+      gridSeven: "",
+      gridEight: "",
+      gridNine: ""
+    })
+    setCurrentPlayer("X")
+    setGameWon(false)
+    setWinner("")
+  }
+
   return (
     <div className="App">
       <nav>
-        <h1>Tic Tac Toe</h1>
+        <h1 onClick={resetGame} className="nav-title">Tic Tac Toe</h1>
+        <a 
+          href="https://github.com/andrew-j-ortiz/tictactoe" 
+          target="_blank" 
+          className='nav-link'
+        >source code</a>
       </nav>
+      <div className="announcer">
+        <h2>
+          {
+            gameWon ? `Game over! ${winner} wins!` : `${currentPlayer}'s turn`
+          }
+        </h2>
+      </div>
       <div className="board">
         {rows()}
       </div>
       <div className='select'>
-        <button>CPU</button>
+        <button 
+          onClick={()=>setCpuEnabled(prevState => !prevState)}
+          className={cpuEnabled ? "cpuEnabled" : ""}
+        >CPU</button>
       </div>
       <footer>
-        <a href="">github</a>
+        <a href="https://github.com/andrew-j-ortiz/tictactoe" target="_blank">github</a>
       </footer>
     </div>
   )
